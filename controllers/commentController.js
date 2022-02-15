71d0267e1ec18d4c70f684324f31973d8225c90d
Piotroszko <piotroszko2@gmail.com>
2022-02-15 15:22:23 +0100

    Comment controller fixed and reimplemented likes functionality

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -6,8 +6,9 @@ const Video = require("../models/Video");
 const config = require("../config/index");
 const Comment = require("../models/Comment");
 const CommentLike = require("../models/CommentLike");
+const User = require("../models/User");
 
-router.get("/all/:id", function (req, res) {
+router.get("/all/:id", function (req, res, next) {
   //get all comment for video
   Video.findOne({ id: req.params.id }, function (error, video) {
     if (error) {
@@ -16,14 +17,72 @@ router.get("/all/:id", function (req, res) {
         .send(`Server error: ${error.message}`);
     }
     if (video) {
-      Comment.find({ videoID: req.params.id }, function (error, comment) {
+      Comment.find({ videoID: req.params.id }, async function (error, comment) {
+        const users = await User.find({
+          $or: comment.map((v) => ({
+            _id: v.userID,
+          })),
+        });
+        const likes = await CommentLike.find({
+          $or: comment.map((v) => ({
+            commentID: v.id,
+          })),
+        });
         if (error) {
           return res
             .status(httpStatus.INTERNAL_SERVER_ERROR)
             .send(`Server error: ${error.message}`);
         }
         if (comment) {
-          return res.status(httpStatus.OK).send(comment);
+          if (req.headers["authorization"] || req.headers["Authorization"]) {
+            verifyToken(req, res, next);
+            const comms = comment.map((com) => {
+              let likeStatus = "none";
+              if (
+                likes
+                  .find((f) => f.commentID === com.id)
+                  ["liked"].includes(req.userId)
+              ) {
+                likeStatus = "like";
+              } else if (
+                likes
+                  .find((f) => f.commentID === com.id)
+                  .disliked.includes(req.userId)
+              ) {
+                likeStatus = "dislike";
+              }
+              return {
+                id: com.id,
+                title: com.title,
+                content: com.content,
+                likes: likes.find((c) => c.commentID == com.id).liked.length,
+                dislikes: likes.find((c) => c.commentID == com.id).disliked
+                  .length,
+                likeStatus,
+                videoID: com.videoID,
+                userID: com.userID,
+                userName: users.find((u) => u.id == com.userID).name,
+              };
+            });
+
+            return res.status(httpStatus.OK).send(comms);
+          } else {
+            const comms = comment.map((com) => {
+              return {
+                id: com.id,
+                title: com.title,
+                content: com.content,
+                likes: likes.find((c) => c.commentID == com.id).liked.length,
+                dislikes: likes.find((c) => c.commentID == com.id).disliked
+                  .length,
+                videoID: com.videoID,
+                userID: com.userID,
+                userName: users.find((u) => u.id == com.userID).name,
+              };
+            });
+
+            return res.status(httpStatus.OK).send(comms);
+          }
         } else {
           return res.status(httpStatus.OK).send(`[]`);
         }
@@ -58,6 +117,7 @@ router.post("/add/:id", verifyToken, function (req, res) {
               .status(httpStatus.INTERNAL_SERVER_ERROR)
               .send({ comment: false, error: message });
           }
+          CommentLike.create({ commentID: comment.id });
           res.status(httpStatus.OK).send({ comment: true, comment });
         }
       );
@@ -70,108 +130,102 @@ router.post("/add/:id", verifyToken, function (req, res) {
 });
 
 router.delete("/del/:id", verifyToken, function (req, res) {
-  Comment.deleteOne(
+  Comment.deleteMany(
     {
       userID: req.userId,
-      commentID: req.params.id,
+      _id: req.params.id,
     },
-    function (error) {
+    async function (error) {
       if (error) {
         const message = `Server error: ${error.message}`;
         return res
           .status(httpStatus.INTERNAL_SERVER_ERROR)
           .send({ comment: false, error: message });
       }
-      CommentLike.deleteMany({ commentID: req.params.id });
+      await CommentLike.deleteMany({ commentID: req.params.id });
       res.status(httpStatus.OK).send({ commentDeleted: true });
     }
   );
 });
 router.get("/like/:id", verifyToken, function (req, res) {
-  CommentLike.findOne(
-    { commentID: req.params.id, userID: req.userId },
-    function (err, item) {
-      if (err) {
-        return res.status(httpStatus.BAD_REQUEST).send(" Server error ");
-      }
-      if (item) {
-        res.status(httpStatus.OK).send({ likeStatus: item.likeStatus });
-      } else {
-        res.status(httpStatus.OK).send({ likeStatus: "none" });
-      }
+  CommentLike.findOne({ commentID: req.params.id }, function (err, item) {
+    if (err) {
+      return res.status(httpStatus.BAD_REQUEST).send(" Server error ");
     }
-  );
+    if (item) {
+      if (item.liked.includes(req.userId)) {
+        return res.status(httpStatus.OK).send({ likeStatus: "like" });
+      } else if (item.disliked.includes(req.userId)) {
+        return res.status(httpStatus.OK).send({ likeStatus: "dislike" });
+      } else {
+        return res.status(httpStatus.OK).send({ likeStatus: "none" });
+      }
+    } else {
+      res.status(httpStatus.OK).send({ likeStatus: "none" });
+    }
+  });
 });
 
 router.post("/like/:id", verifyToken, function (req, res) {
-  CommentLike.updateOne(
-    { commentID: req.params.id, userID: req.userId },
-    { commentID: req.params.id, userID: req.userId, likeStatus: "like" },
-    { upsert: true },
-    function (err, response) {
-      if (err) {
-        return res.status(httpStatus.BAD_REQUEST).send(" Server error ");
-      }
-      if (response) {
-        Comment.findOne({ _id: req.params.id }, function (err, comment) {
-          comment.likes = comment.likes + 1;
-          comment.save();
-        });
-        res.status(httpStatus.OK).send({ likeStatus: "like" });
-      } else {
-        return res.status(httpStatus.BAD_REQUEST).send(" Bad request ");
-      }
+  CommentLike.findOne({ commentID: req.params.id }, function (err, item) {
+    if (err) {
+      return res.status(httpStatus.BAD_REQUEST).send(" Server error ");
     }
-  );
-});
-router.post("/unlike/:id", verifyToken, function (req, res) {
-  CommentLike.findOne(
-    { commentID: req.params.id, userID: req.userId },
-    function (err, comLike) {
-      if (err) {
-        return res.status(httpStatus.BAD_REQUEST).send(" Server error ");
-      }
-      if (response) {
-        if (comLike.likeStatus == "like") {
-          Comment.findOne({ _id: req.params.id }, function (err, comment) {
-            comment.likes = comment.likes - 1;
-            comment.save();
-          });
-        } else if (comLike.likeStatus == "dislike") {
-          Comment.findOne({ _id: req.params.id }, function (err, comment) {
-            comment.dislikes = comment.dislikes - 1;
-            comment.save();
-          });
-        }
-        comLike.likeStatu = "none";
-        comLike.save();
-        res.status(httpStatus.OK).send({ likeStatus: "none" });
+    if (item) {
+      if (item.liked.includes(req.userId)) {
+        return res.status(httpStatus.OK).send({ likeStatus: "like" });
+      } else if (item.disliked.includes(req.userId)) {
+        item.disliked = item.disliked.filter((i) => i === req.userId);
+        item.liked.push(req.userId);
+        item.save();
+        return res.status(httpStatus.OK).send({ likeStatus: "like" });
       } else {
-        return res.status(httpStatus.BAD_REQUEST).send(" Bad request ");
+        item.liked.push(req.userId);
+        item.save();
+        return res.status(httpStatus.OK).send({ likeStatus: "like" });
       }
+    } else {
+      return res.status(httpStatus.BAD_REQUEST).send(" Bad request ");
     }
-  );
+  });
 });
 router.post("/dislike/:id", verifyToken, function (req, res) {
-  CommentLike.updateOne(
-    { commentID: req.params.id, userID: req.userId },
-    { commentID: req.params.id, userID: req.userId, likeStatus: "dislike" },
-    { upsert: true },
-    function (err, response) {
-      if (err) {
-        return res.status(httpStatus.BAD_REQUEST).send(" Server error ");
-      }
-      if (response) {
-        Comment.findOne({ _id: req.params.id }, function (err, comment) {
-          comment.dislikes = comment.dislikes + 1;
-          comment.save();
-        });
-        res.status(httpStatus.OK).send({ likeStatus: "dislike" });
-      } else {
-        return res.status(httpStatus.BAD_REQUEST).send(" Bad request ");
-      }
+  CommentLike.findOne({ commentID: req.params.id }, function (err, item) {
+    if (err) {
+      return res.status(httpStatus.BAD_REQUEST).send(" Server error ");
     }
-  );
+    if (item) {
+      if (item.disliked.includes(req.userId)) {
+        return res.status(httpStatus.OK).send({ likeStatus: "dislike" });
+      } else if (item.liked.includes(req.userId)) {
+        item.liked = item.liked.filter((i) => i === req.userId);
+        item.disliked.push(req.userId);
+        item.save();
+        return res.status(httpStatus.OK).send({ likeStatus: "dislike" });
+      } else {
+        item.disliked.push(req.userId);
+        item.save();
+        return res.status(httpStatus.OK).send({ likeStatus: "dislike" });
+      }
+    } else {
+      return res.status(httpStatus.BAD_REQUEST).send(" Bad request ");
+    }
+  });
+});
+router.post("/unlike/:id", verifyToken, function (req, res) {
+  CommentLike.findOne({ commentID: req.params.id }, function (err, item) {
+    if (err) {
+      return res.status(httpStatus.BAD_REQUEST).send(" Server error ");
+    }
+    if (item) {
+      item.liked = item.liked.filter((i) => i === req.userId);
+      item.disliked = item.disliked.filter((i) => i === req.userId);
+      item.save();
+      return res.status(httpStatus.OK).send({ likeStatus: "none" });
+    } else {
+      return res.status(httpStatus.BAD_REQUEST).send(" Bad request ");
+    }
+  });
 });
 
 module.exports = router;
--- models/CommentLike.js
+++ models/CommentLike.js
@@ -3,20 +3,22 @@ mongoose.set("useFindAndModify", false);
 const Schema = mongoose.Schema;
 
 const commentLikeSchema = new Schema({
-  userID: {
-    required: true,
-    type: String,
-  },
   commentID: {
     required: true,
     type: String,
   },
-  likeStatus: {
-    type: String,
-    enum: ["like", "dislike", "none"],
-    default: "none",
-    required: true,
-  },
+  liked: [
+    {
+      type: Schema.ObjectId,
+      ref: "User",
+    },
+  ],
+  disliked: [
+    {
+      type: Schema.ObjectId,
+      ref: "User",
+    },
+  ],
 });
 
 commentLikeSchema.set("toJSON", { virtuals: true });
